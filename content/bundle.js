e150b74d339c28391f1948ab77618dd565cc86bc
Mackenzie Hartman <mackenzie.e.hartman@gmail.com>
2017-07-06 14:55:09 -0400

    ugh

--- content/bundle.js
+++ content/bundle.js
@@ -146,6 +146,60 @@
 		
 
 (function(){
+	angular.module('workoutlog.define', [
+		'ui.router'
+	])
+	.config(defineConfig);
+
+	function defineConfig($stateProvider){
+
+		$stateProvider
+			.state('define', {
+				url: '/define',
+				templateUrl: '/components/define/define.html',
+				controller: DefineController,
+				controllerAs: 'ctrl',
+				bindToController: this,
+				resolve: [
+					'CurrentUser', '$q', '$state',
+					function(CurrentUser, $q, $state){
+						var deferred = $q.defer();
+						if (CurrentUser.isSignedIn()){
+							deferred.resolve();
+						} else {
+							deferred.reject();
+							$state.go('signin');
+						}
+						return deferred.promise;
+					}
+				]	
+			});
+	}
+	
+	defineConfig.$inject = [ '$stateProvider'];
+
+	function DefineController($state, DefineService){
+		var vm = this;
+		vm.message = "Define a workout category here";
+		vm.saved = false;
+		vm.definition = {};
+		vm.save = function(){
+			DefineService.save(vm.definition)
+				.then(function(){
+					vm.saved = true;
+					$state.go('logs')
+				});
+		};
+	}
+	DefineController.$inject = [ '$state', 'DefineService'];
+})();
+
+//  Resolve is built into Angular as a function that
+// executes code prior to going to that route.  In this instance, the resolve is running functions to
+// ensure that a user is actually logged in.  To do this, $q is injected (Angular’s way to build custom
+// promises), $state is injected and CurrentUser.
+
+(function(){
 	angular.module('workoutlog.history',[
 		'ui.router'
 	])
@@ -272,60 +326,6 @@
 // Notice on the .state(‘logs/update’) that there are two functions that occur on the resolve.  This
 // allows the route to have access to the data of the log being edited.  Also note, that the resolve is
 // getting all user definitions of a workout
-
-(function(){
-	angular.module('workoutlog.define', [
-		'ui.router'
-	])
-	.config(defineConfig);
-
-	function defineConfig($stateProvider){
-
-		$stateProvider
-			.state('define', {
-				url: '/define',
-				templateUrl: '/components/define/define.html',
-				controller: DefineController,
-				controllerAs: 'ctrl',
-				bindToController: this,
-				resolve: [
-					'CurrentUser', '$q', '$state',
-					function(CurrentUser, $q, $state){
-						var deferred = $q.defer();
-						if (CurrentUser.isSignedIn()){
-							deferred.resolve();
-						} else {
-							deferred.reject();
-							$state.go('signin');
-						}
-						return deferred.promise;
-					}
-				]	
-			});
-	}
-	
-	defineConfig.$inject = [ '$stateProvider'];
-
-	function DefineController($state, DefineService){
-		var vm = this;
-		vm.message = "Define a workout category here";
-		vm.saved = false;
-		vm.definition = {};
-		vm.save = function(){
-			DefineService.save(vm.definition)
-				.then(function(){
-					vm.saved = true;
-					$state.go('logs')
-				});
-		};
-	}
-	DefineController.$inject = [ '$state', 'DefineService'];
-})();
-
-//  Resolve is built into Angular as a function that
-// executes code prior to going to that route.  In this instance, the resolve is running functions to
-// ensure that a user is actually logged in.  To do this, $q is injected (Angular’s way to build custom
-// promises), $state is injected and CurrentUser.
 
 (function(){
 	angular.module('workoutlog')
