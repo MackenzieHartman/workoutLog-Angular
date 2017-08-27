9e7c85877c5aa28fbaa851a570e0d54df3409e02
Mackenzie Hartman <mackenzie.e.hartman@gmail.com>
2017-08-27 00:41:30 -0400

    styled app

--- content/bundle.js
+++ content/bundle.js
@@ -146,6 +146,52 @@
 		
 
 (function(){
+	angular.module('workoutlog.history',[
+		'ui.router'
+	])
+	.config(historyConfig);
+	historyConfig.$inject = ['$stateProvider'];
+	function historyConfig($stateProvider){
+
+		$stateProvider
+			.state('history', {
+				url: '/history',
+				templateUrl: '/components/history/history.html',
+				controller: HistoryController,
+				controllerAs: 'ctrl',
+				bindToController: this,
+				resolve: {
+					getUserLogs: [
+						'LogsService',
+						function(LogsService){
+							return LogsService.fetch();
+						}
+					]
+				}
+			});
+	}
+	
+	HistoryController.$inject = ['$state', 'LogsService'];
+	function HistoryController($state, LogsService){
+		var vm= this;
+		vm.history = LogsService.getLogs();
+
+		vm.delete = function(item){
+			LogsService.deleteLogs(item);
+		};
+
+		vm.updateLog = (function(item){
+			$state.go('logs/update', {'id' : item.id});
+			});
+	}
+})();
+
+
+// Notice how LogsService is injected and then implemented in this controller.  The history
+// component is used to present the collection of logs.  Look inside vm.updateLog, $state.go has the
+// route as the first argument but the second argument is an object with an id property.  This is how logs.js ‘knows” which log to get so it can be updated.
+
+(function(){
 	angular.module('workoutlog.define', [
 		'ui.router'
 	])
@@ -198,52 +244,6 @@
 // executes code prior to going to that route.  In this instance, the resolve is running functions to
 // ensure that a user is actually logged in.  To do this, $q is injected (Angular’s way to build custom
 // promises), $state is injected and CurrentUser.
-
-(function(){
-	angular.module('workoutlog.history',[
-		'ui.router'
-	])
-	.config(historyConfig);
-	historyConfig.$inject = ['$stateProvider'];
-	function historyConfig($stateProvider){
-
-		$stateProvider
-			.state('history', {
-				url: '/history',
-				templateUrl: '/components/history/history.html',
-				controller: HistoryController,
-				controllerAs: 'ctrl',
-				bindToController: this,
-				resolve: {
-					getUserLogs: [
-						'LogsService',
-						function(LogsService){
-							return LogsService.fetch();
-						}
-					]
-				}
-			});
-	}
-	
-	HistoryController.$inject = ['$state', 'LogsService'];
-	function HistoryController($state, LogsService){
-		var vm= this;
-		vm.history = LogsService.getLogs();
-
-		vm.delete = function(item){
-			LogsService.deleteLogs(item);
-		};
-
-		vm.updateLog = (function(item){
-			$state.go('logs/update', {'id' : item.id});
-			});
-	}
-})();
-
-
-// Notice how LogsService is injected and then implemented in this controller.  The history
-// component is used to present the collection of logs.  Look inside vm.updateLog, $state.go has the
-// route as the first argument but the second argument is an object with an id property.  This is how logs.js ‘knows” which log to get so it can be updated.
 
 (function(){
 	angular.module('workoutlog.logs',[
